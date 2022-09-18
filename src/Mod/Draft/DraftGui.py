b1f5636c15e1b6aea4644bc2e6ca17a672368697
Andrés Barreiro <andres.bga@gmail.com>
2022-09-18 23:46:27 +0200

    Remove Drawing workbench

--- src/Mod/Draft/DraftGui.py
+++ src/Mod/Draft/DraftGui.py
@@ -2180,8 +2180,7 @@ class DraftToolBar:
                 self.commands = ["Draft_Move","Draft_Rotate",
                                  "Draft_Scale","Draft_Offset",
                                  "Draft_Trimex","Draft_Upgrade",
-                                 "Draft_Downgrade","Draft_Edit",
-                                 "Draft_Drawing"]
+                                 "Draft_Downgrade","Draft_Edit"]
                 self.title = "Modify objects"
             def shouldShow(self):
                 return (FreeCAD.ActiveDocument is not None) and (FreeCADGui.Selection.getSelection() != [])
